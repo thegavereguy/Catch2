42e8cf5c3fb61a02cbffab590cfa3bdc7a859f7d
thegavereguy <dallacia.enrico@protonmail.com>
2024-11-16 18:17:49 +0100

    testing addition of benchmark stats

--- src/catch2/benchmark/detail/catch_benchmark_stats.hpp
+++ src/catch2/benchmark/detail/catch_benchmark_stats.hpp
@@ -13,36 +13,35 @@
 // The fwd decl & default specialization needs to be seen by VS2017 before
 // BenchmarkStats itself, or VS2017 will report compilation error.
 #include <catch2/benchmark/detail/catch_benchmark_stats_fwd.hpp>
-
 #include <string>
 #include <vector>
 
 namespace Catch {
 
-    struct BenchmarkInfo {
-        std::string name;
-        double estimatedDuration;
-        int iterations;
-        unsigned int samples;
-        unsigned int resamples;
-        double clockResolution;
-        double clockCost;
-    };
+struct BenchmarkInfo {
+  std::string name;
+  double estimatedDuration;
+  int iterations;
+  unsigned int samples;
+  unsigned int resamples;
+  double clockResolution;
+  double clockCost;
+};
 
-    // We need to keep template parameter for backwards compatibility,
-    // but we also do not want to use the template paraneter.
-    template <class Dummy>
-    struct BenchmarkStats {
-        BenchmarkInfo info;
+// We need to keep template parameter for backwards compatibility,
+// but we also do not want to use the template paraneter.
+template <class Dummy>
+struct BenchmarkStats {
+  BenchmarkInfo info;
 
-        std::vector<Benchmark::FDuration> samples;
-        Benchmark::Estimate<Benchmark::FDuration> mean;
-        Benchmark::Estimate<Benchmark::FDuration> standardDeviation;
-        Benchmark::OutlierClassification outliers;
-        double outlierVariance;
-    };
+  std::vector<Benchmark::FDuration> samples;
+  Benchmark::Estimate<Benchmark::FDuration> mean;
+  Benchmark::Estimate<Benchmark::FDuration> standardDeviation;
+  Benchmark::OutlierClassification outliers;
+  double outlierVariance;
+  int test;
+};
 
+}  // end namespace Catch
 
-} // end namespace Catch
-
-#endif // CATCH_BENCHMARK_STATS_HPP_INCLUDED
+#endif  // CATCH_BENCHMARK_STATS_HPP_INCLUDED
